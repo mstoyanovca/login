d28c806f9bbaf6fb9abba181e1a1e6093c129ebd
mstoyanovca <mstoyanovca@gmail.com>
2025-04-25 20:03:39 -0400

    upgraded to Angular 18

--- front-end-2/src/app/app.module.ts
+++ front-end-2/src/app/app.module.ts
@@ -1,7 +1,7 @@
 ﻿import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 // used to create fake backend
 import { fakeBackendProvider } from './_helpers';
@@ -13,26 +13,19 @@ import { AppComponent } from './app.component';
 import { AlertComponent } from './_components';
 import { HomeComponent } from './home';
 
-@NgModule({
-    imports: [
-        BrowserModule,
-        ReactiveFormsModule,
-        HttpClientModule,
-        AppRoutingModule
-    ],
-    declarations: [
+@NgModule({ declarations: [
         AppComponent,
         AlertComponent,
         HomeComponent
     ],
-    providers: [
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        ReactiveFormsModule,
+        AppRoutingModule], providers: [
         { provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [AccountService] },
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-
         // provider used to create fake backend
-        fakeBackendProvider
-    ],
-    bootstrap: [AppComponent]
-})
+        fakeBackendProvider,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
