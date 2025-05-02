c70a418ba814d87df6670b9bca70dc800af9349c
mstoyanovca <mstoyanovca@gmail.com>
2025-05-01 23:54:09 -0400

    removed first component

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 
-import { FirstComponent } from '@component/first/first.component';
-import { SecondComponent } from '@component/second/second.component';
 import { LoginComponent } from '@component/login/login.component';
 import { PageNotFoundComponent } from '@component/page-not-found/page-not-found.component';
 
@@ -9,7 +7,5 @@ export const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full' },
   { path: 'home', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'first-component', component: FirstComponent },
-  { path: 'second-component', component: SecondComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
