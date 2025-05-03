fcb93f522195145a5a5e1ffd68c11dce4a6071d2
mstoyanovca <mstoyanovca@gmail.com>
2025-05-03 16:36:36 -0400

    completed login form

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -10,6 +10,6 @@ export const routes: Routes = [
   { path: 'home', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'forgot-password', component: RegisterComponent },
+  { path: 'forgotPassword', component: ForgotPasswordComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
