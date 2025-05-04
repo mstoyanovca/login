7453bc3f6d46678471d642d46cb7ac648d6880cf
mstoyanovca <mstoyanovca@gmail.com>
2025-05-03 20:50:19 -0400

    added eye slash

--- front-end/src/app/component/register/register.component.ts
+++ front-end/src/app/component/register/register.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, FontAwesomeModule],
   templateUrl: 'register.component.html',
   styleUrl: 'register.component.css'
 })
 export class RegisterComponent {
+  show: boolean = false;
 
+  showPassword() {
+    this.show = !this.show;
+  }
 }
