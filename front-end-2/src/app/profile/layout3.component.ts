ece9688a1ffa0ec06b0ba154b9d4e5bc51809f84
mstoyanovca <mstoyanovca@gmail.com>
2025-05-10 21:23:22 -0400

    deleted front-end-2

--- front-end-2/src/app/profile/layout3.component.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-@Component({
-    selector: 'layout3',
-    templateUrl: 'layout3.component.html',
-    imports: [RouterOutlet]
-})
-export class Layout3Component { }
