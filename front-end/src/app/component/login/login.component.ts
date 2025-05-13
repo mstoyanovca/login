4ad4c24e6d23f8e014468c9c60ea7fa3d14c8e85
mstoyanovca <mstoyanovca@gmail.com>
2025-05-13 19:54:19 -0400

    deleted JwtService from Angular

--- front-end/src/app/component/login/login.component.ts
+++ front-end/src/app/component/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { JwtService } from '@service/jwt/jwt.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { User } from '@model/user';
@@ -16,7 +15,6 @@ import { User } from '@model/user';
 export class LoginComponent {
     showPassword = false;
     user = new User(0, '', '', '', '', 'admin', false);
-    private jwtService = inject(JwtService);
     private httpClient = inject(HttpClient);
 
     onSpanClick() {
@@ -24,6 +22,8 @@ export class LoginComponent {
     }
 
     onSubmit() {
-        this.httpClient.post('http://localhost:8080/login', this.user, { responseType: 'text' }).subscribe(jwt => console.log('jwt = ', jwt));
+        this.httpClient
+            .post('http://localhost:8080/login', this.user, { responseType: 'text' })
+            .subscribe(jwt => console.log('jwt = ', jwt));
     }
 }
