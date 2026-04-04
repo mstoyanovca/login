406a3f06201a6551a4d55c2ff972a6d3b34d6e75
mstoyanovca <mstoyanovca@gmail.com>
2026-04-04 16:08:48 -0400

    disabled logs in prod

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,7 +1,20 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-
-import { appConfig } from '@app/app.config';
-import { AppComponent } from '@app/app.component';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {appConfig} from '@app/app.config';
+import {enableProdMode} from '@angular/core';
+import {environment} from '@environment/environment';
+import {AppComponent} from '@app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
     .catch((err) => console.error(err));
+
+if (environment.production) {
+    enableProdMode();
+    window.console.log = () => {
+    };
+    window.console.info = () => {
+    };
+    window.console.warn = () => {
+    };
+    window.console.debug = () => {
+    };
+}
