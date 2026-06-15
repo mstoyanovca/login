251e78b9336ce9622dec69fa96e35d4b091be62b
mstoyanovca <mstoyanovca@gmail.com>
2026-06-14 21:15:18 -0400

    created AccountControllerTest

--- src/test/java/login/controller/AccountControllerTest.java
+++ src/test/java/login/controller/AccountControllerTest.java
@@ -10,18 +10,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import tools.jackson.databind.ObjectMapper;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ContextConfiguration
@@ -30,6 +35,8 @@ public class AccountControllerTest {
     @Autowired
     private WebApplicationContext context;
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockitoBean
     private UserRepository userRepository;
@@ -38,6 +45,9 @@ public class AccountControllerTest {
 
     @MockitoBean
     private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    private final User user = new User(1L, "Martin", "Stoyanov", "a@a.com", "password", Role.USER, false);
+
 
     @BeforeEach
     public void setup() {
@@ -51,12 +61,10 @@ public class AccountControllerTest {
 
     @Test
     @WithMockUser(username = "a@a.com", roles = {"USER", "ADMIN"})
-    public void accountTest() throws Exception {
-        final User user = new User(1L, "Martin", "Stoyanov", "a@a.com", "password", Role.USER, false);
+    public void accountAuthorizedTest() throws Exception {
         when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
 
-        mockMvc
-                .perform(get("/account")
+        mockMvc.perform(get("/account")
                         .accept(MediaType.APPLICATION_JSON)
                 )
                 .andExpect(status().isOk())
@@ -68,5 +76,56 @@ public class AccountControllerTest {
                 .andExpect(jsonPath("$.password").value(user.getPassword()))
                 .andExpect(jsonPath("$.role").value(user.getRole().toString()))
                 .andExpect(jsonPath("$.enabled").value(user.isEnabled()));
+    }
+
+    @Test
+    @WithMockUser(username = "a@a.com", roles = {"USER", "ADMIN"})
+    public void accountAuthorizedNotFoundTest() throws Exception {
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.empty());
+
+        mockMvc.perform(get("/account")
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isInternalServerError());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void updateUnauthorizedTest() throws Exception {
+        mockMvc.perform(get("/account")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "a@a.com", roles = {"USER", "ADMIN"})
+    public void updateAuthorizedTest() throws Exception {
+        when(passwordEncoder.encode(user.getPassword())).thenReturn(UUID.randomUUID().toString());
+
+        mockMvc
+                .perform(put("/update")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user))
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(user.getId()))
+                .andExpect(jsonPath("$.firstName").value(user.getFirstName()))
+                .andExpect(jsonPath("$.lastName").value(user.getLastName()))
+                .andExpect(jsonPath("$.email").value(user.getEmail()))
+                .andExpect(jsonPath("$.password").value(""))
+                .andExpect(jsonPath("$.role").value(user.getRole().toString()))
+                .andExpect(jsonPath("$.enabled").value(user.isEnabled()));
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void accountUnauthorizedTest() throws Exception {
+        mockMvc.perform(put("/update")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user))
+        );
     }
 }
