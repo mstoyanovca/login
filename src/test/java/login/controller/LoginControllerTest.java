cf8ebb7b9c80984c1014e77ff7285113da058cf7
mstoyanovca <mstoyanovca@gmail.com>
2026-06-16 22:04:06 -0400

    completed LoginControllerTest

---
+++ src/test/java/login/controller/LoginControllerTest.java
@@ -0,0 +1,97 @@
+package login.controller;
+
+import login.WebSecurityConfiguration;
+import login.model.Role;
+import login.model.User;
+import login.repository.UserRepository;
+import login.service.JwtService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import tools.jackson.databind.ObjectMapper;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ContextConfiguration
+@Import(WebSecurityConfiguration.class)
+@WebMvcTest(LoginController.class)
+public class LoginControllerTest {
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private JwtService jwtService;
+    @MockitoBean
+    private AuthenticationManager authManager;
+
+    @MockitoBean
+    private UserRepository userRepository;
+    @MockitoBean
+    private UserDetailsService userDetailsService;
+
+    private final User user = new User(45L, "Martin", "Stoyanov", "efg@lmn.com", "password", Role.USER, true);
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void loginExistingUserTest() throws Exception {
+        when(authManager.authenticate(any())).thenReturn(mock(Authentication.class));
+        when(jwtService.generate(user.getEmail())).thenReturn("jwt_token");
+
+        mockMvc
+                .perform(post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user))
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/plain;charset=UTF-8"))
+                .andExpect(content().string("jwt_token"));
+
+        verify(authManager, times(1)).authenticate(any(UsernamePasswordAuthenticationToken.class));
+        verify(jwtService, times(1)).generate(user.getEmail());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void loginUnknownUserTest() throws Exception {
+        doThrow(new BadCredentialsException("Invalid password")).when(authManager).authenticate(any(UsernamePasswordAuthenticationToken.class));
+
+        mockMvc
+                .perform(post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user))
+                )
+                .andExpect(status().isUnauthorized());
+
+        verify(authManager, times(1)).authenticate(any(UsernamePasswordAuthenticationToken.class));
+        verifyNoInteractions(jwtService);
+    }
+}
